5b687cb1164e37a2e286e49ca195982630239bf0
Georgia Kesisoglou <georgiakes@gmail.com>
2026-06-16 18:01:43 +0200

    Remove metrics.py

--- bin/rep.py
+++ bin/rep.py
@@ -24,23 +24,22 @@
 #     layout as:
 #
 #       bin/
-#         rep.py                        <- this file
-#         ga4gh_report_assets/
-#           report/
-#             metrics.py               <- GA4GH Python helper (unchanged)
-#           html/
-#             report.jinja2.html
-#             roc_plot.jinja2.html
-#             metrics_table.jinja2.html
-#             helpers.js
-#             roc.js
-#             report.css               <- includes vendored bijou CSS
+#         rep.py                        <- this file (GA4GH metrics logic inlined)
+#       assets/
+#         html/
+#           report.jinja2.html
+#           roc_plot.jinja2.html
+#           metrics_table.jinja2.html
+#           helpers.js
+#           roc.js
+#           report.css               <- includes vendored bijou CSS
 #
 #     No lib/bijou/ or lib/d3/ directory is needed anywhere.
 
 import sys
 import os
 import json
+import math
 import gzip
 import copy
 import argparse
@@ -52,13 +51,186 @@ import jinja2
 # Path setup
 # ---------------------------------------------------------------------------
 SCRIPTDIR   = os.path.abspath(os.path.dirname(__file__))
-ASSETDIR    = os.path.join(SCRIPTDIR, "ga4gh_report_assets")
-TEMPLATEDIR = os.path.join(ASSETDIR, "html")
-LIBDIR      = ASSETDIR  # report/metrics.py lives at ga4gh_report_assets/report/metrics.py
+PROJECTDIR  = os.path.dirname(SCRIPTDIR)
+TEMPLATEDIR = os.path.join(PROJECTDIR, "assets", "html")
 
-sys.path.insert(0, LIBDIR)
+# ---------------------------------------------------------------------------
+# GA4GH metrics (inlined from the former ga4gh_report_assets/report/metrics.py)
+# ---------------------------------------------------------------------------
 
-import report.metrics
+class Metrics:
+    """ single data point for metrics """
+
+    METRIC_NAMES = [
+        "QQ",
+        "TRUTH.TP",
+        "TRUTH.FN",
+        "QUERY.FP",
+        "QUERY.UNK",
+        "TRUTH.TOTAL",
+        "QUERY.TOTAL",
+        "FP.gt",
+        "FP.al",
+        "METRIC.Recall",
+        "METRIC.Precision",
+        "METRIC.F1_Score",
+        "METRIC.Frac_NA",
+        "TRUTH.TOTAL.TiTv_ratio",
+        "TRUTH.TOTAL.het_hom_ratio",
+        "QUERY.TOTAL.TiTv_ratio",
+        "QUERY.TOTAL.het_hom_ratio",
+    ]
+
+    METRIC_INDEX = {n: i for i, n in enumerate(METRIC_NAMES)}
+
+    def __init__(self, **kwargs):
+        self.metrics = [0 for _ in range(len(Metrics.METRIC_NAMES))]
+
+        def safe_float(x):
+            try:
+                return float(x)
+            except Exception:
+                return float("nan")
+
+        for k, v in kwargs.items():
+            self.metric(k, safe_float(v))
+
+        try:
+            r = self.metric("METRIC.Recall")
+            p = self.metric("METRIC.Precision")
+            self.metric("METRIC.F1_Score", 2.0 * p * r / (p + r))
+        except Exception:
+            pass
+
+    def metric(self, name, value=None):
+        if value is not None:
+            self.metrics[Metrics.METRIC_INDEX[name]] = value
+            return value
+        return self.metrics[Metrics.METRIC_INDEX[name]]
+
+    def to_dict(self):
+        result = {}
+        for i, m in enumerate(Metrics.METRIC_NAMES):
+            result[m] = "nan" if math.isnan(self.metrics[i]) else self.metrics[i]
+        return result
+
+
+class MetricSet:
+    """ Set of metrics that belong together with a ROC and an aggregate result """
+
+    def __init__(self, method, cmethod, vartype, subtype, genotype, varfilter, subset, qq_field):
+        self.method = method
+        self.comparisonmethod = cmethod
+        self.type = vartype
+        self.subtype = subtype
+        self.genotype = genotype
+        self.filter = varfilter
+        self.subset = subset
+        self.qq_field = qq_field
+        self.roc = []
+        self.aggregate = None
+
+    def clip_roc(self, metrics, diff, max_data_points=1000, minmax=None):
+        roc = self.roc
+        if not roc:
+            return roc
+        sorted_roc = sorted(roc, key=lambda point: point.metric("QQ"))
+        last = sorted_roc[0]
+        croc = [last]
+        for x in sorted_roc[1:]:
+            if len(croc) > max_data_points:
+                break
+            ignore = False
+            for m in metrics:
+                met = x.metric(m)
+                if minmax and m in minmax:
+                    if "min" in minmax[m] and met < minmax[m]["min"]:
+                        ignore = True
+                        break
+                    if "max" in minmax[m] and met > minmax[m]["max"]:
+                        ignore = True
+                        break
+            if ignore:
+                continue
+            abs_diff = max(abs(last.metric(m) - x.metric(m)) for m in metrics)
+            if abs_diff > diff:
+                last = x
+                croc.append(x)
+        self.roc = croc
+        return croc
+
+    def to_dict(self):
+        result = {
+            "method": self.method,
+            "comparisonmethod": self.comparisonmethod,
+            "type": self.type,
+            "subtype": self.subtype,
+            "genotype": self.genotype,
+            "filter": self.filter,
+            "subset": self.subset,
+            "qq_field": self.qq_field,
+            "roc": [r.to_dict() for r in self.roc],
+        }
+        result.update(self.aggregate.to_dict())
+        return result
+
+
+def read_qfy_csv(files, method=None, cmethod=None, roc_metrics=None,
+                 roc_diff=0.001, max_data_points=1000, minmax=None):
+    """ Read a metrics CSV file from hap.py / quantify """
+    import csv as _csv
+
+    results = {}
+    if not roc_metrics:
+        roc_metrics = ["METRIC.Recall", "METRIC.Precision"]
+
+    for csv_file_name in files:
+        if csv_file_name.endswith(".gz"):
+            csvfile = gzip.open(csv_file_name, "rt", newline="")
+        else:
+            csvfile = open(csv_file_name, "rt", newline="")
+        try:
+            dialect = _csv.Sniffer().sniff(csvfile.read(8192))
+            csvfile.seek(0)
+            dr = _csv.DictReader(csvfile, dialect=dialect)
+        except Exception:
+            dr = _csv.DictReader(csvfile, dialect="excel")
+
+        if not method:
+            tmethod = os.path.basename(csv_file_name)
+            for suffix in (".gz", ".csv", ".extended.csv", ".roc.Locations"):
+                tmethod = tmethod.replace(suffix, "")
+        else:
+            tmethod = method
+
+        key_list = ["Type", "Subtype", "Genotype", "Filter", "Subset", "QQ.Field"]
+        for line in dr:
+            try:
+                key = ":".join(list(map(str, [cmethod, tmethod])) + [line[k] for k in key_list])
+            except Exception:
+                raise Exception("Misformatted input file: %s" % csv_file_name)
+            if key not in results:
+                results[key] = MetricSet(
+                    tmethod, cmethod,
+                    line["Type"], line["Subtype"], line["Genotype"],
+                    line["Filter"], line["Subset"], line["QQ.Field"],
+                )
+            mvalues = {n: line[n] for n in Metrics.METRIC_NAMES}
+            mobj = Metrics(**mvalues)
+            if line["QQ"] == "*":
+                if results[key].aggregate is not None:
+                    raise Exception("Duplicate aggregate result for %s / %s" % (cmethod, key))
+                results[key].aggregate = mobj
+            else:
+                results[key].roc.append(mobj)
+
+    for key, v in results.items():
+        if not v.aggregate:
+            raise Exception("Missing aggregate result for %s / %s" % (cmethod, key))
+        v.clip_roc(roc_metrics, roc_diff, max_data_points, minmax)
+
+    return [results[k] for k in sorted(results.keys())]
+
 
 # ---------------------------------------------------------------------------
 # CDN asset map
@@ -256,7 +428,7 @@ def main() -> None:
             file=sys.stderr,
         )
 
-        row_metrics = report.metrics.read_qfy_csv(
+        row_metrics = read_qfy_csv(
             rfiles,
             method=method_label,
             cmethod=cmethod_label,
